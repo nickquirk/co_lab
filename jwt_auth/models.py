c592dcd1b7ad231fae63076b154c45811be901b5
Nick Quirk <n.quirk87@gmail.com>
2022-12-16 17:14:11 +0000

    updated user model

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -5,9 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
   email = models.CharField(max_length=50, unique=True)
   username = models.CharField(max_length=50, unique=True)
-  first_name = models.CharField(max_length=50)
-  last_name = models.CharField(max_length=50)
   profile_image = models.CharField(max_length=300, default=None, blank=True, null=True)
   # Method to return User data as a string
   def __str__(self):
-    return f"{self.email} : {self.username} : {self.first_name} : {self.last_name}"
+    return f"{self.email} : {self.username}"

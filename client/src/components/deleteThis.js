2ba6a8145c0f30861432ba6826596cb156b9bb4d
Nick Quirk <n.quirk87@gmail.com>
2023-01-15 18:22:47 +0000

    added README

---
+++ client/src/components/deleteThis.js
@@ -0,0 +1,189 @@
+
+
+
+[[[],[[589,[53],0.125]]],[[],[[245,[48],0.125],[589,[53],0.125]]]]
+
+
+
+
+
+[[[],[]],[[],[]],[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]]
+
+
+PlayChordNow(1, [60], 2/16)
+
+
+
+[ [ [drums], [ [instrument, [notes], duration], [instrument2, [notes], duration] ] ] ]
+
+
+// 
+[{"isChecked":true,
+"drums":[],
+"note":["589",[53],0.125],
+"midiTranspose":54,
+"instrumentGridSize":{"rows":14,"cols":16},
+"tempo":120,
+"instrument":"589",
+"trackType":"instrument"}
+
+
+
+,{"isChecked":true,"drums":[],"note":["589",[53],0.125],"midiTranspose":54,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":true,"drums":[],"note":["589",[49],0.125],"midiTranspose":54,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":true,"drums":[],"note":["589",[49],0.125],"midiTranspose":54,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"},{"isChecked":false,"instrumentGridSize":{"rows":14,"cols":16},"tempo":120,"midiTranspose":54,"instrument":"589","trackType":"instrument"}]
+
+
+
+
+
+
+
+  [
+      [],
+      [
+          [
+              "245",
+              [
+                  48
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      []
+  ],
+  [
+      [],
+      [
+          [
+              "245",
+              [
+                  48
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      [
+          [
+              1,
+              [
+                  49
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      []
+  ],
+  [
+      [],
+      []
+  ],
+  [
+      [],
+      [
+          [
+              "245",
+              [
+                  48
+              ],
+              0.125
+          ],
+          [
+              "589",
+              [
+                  49
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      [
+          [
+              "245",
+              [
+                  48
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      [
+          [
+              1,
+              [
+                  52
+              ],
+              0.125
+          ],
+          [
+              "589",
+              [
+                  49
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      []
+  ],
+  [
+      [],
+      [
+          [
+              "245",
+              [
+                  48
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      [
+          [
+              1,
+              [
+                  50
+              ],
+              0.125
+          ],
+          [
+              "245",
+              [
+                  48
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      [
+          [
+              "245",
+              [
+                  48
+              ],
+              0.125
+          ]
+      ]
+  ],
+  [
+      [],
+      []
+  ]
+]
